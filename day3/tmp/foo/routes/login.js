b4d3ecdc3f2147a6a8896f88046354ba7d70c365
lijichao <1159165064@qq.com>
2019-10-10 20:54:21 +0800

    尝试nodejs_mysql失败

--- day3/tmp/foo/routes/login.js
+++ day3/tmp/foo/routes/login.js
@@ -15,31 +15,19 @@ router.post('/', function (req, res, next) {
         password
     } = req.body;
     SelectUser
-    // if (username == "qwe" && password == "asd") {
-    //     res.json({
-    //         status: 1,
-    //         msg: "登录成功"
-    //     });
-    // } else {
-    //     res.json({
-    //         status: 0,
-    //         msg: "登录失败"
-    //     })
-    // }
+    if (username == "qwe" && password == "asd") {
+        res.json({
+            status: 1,
+            msg: "登录成功"
+        });
+    } else {
+        res.json({
+            status: 0,
+            msg: "登录失败"
+        })
+    }
 
 });
 
-function SelectUser(username, password) {
-    pool.getConnection(function (err, connection) {
-        if (err) throw err;
-        connection.query(`SELECT username,password from user where user=${username} and password=${password}`, function (error, res, fields) {
-
-            user = res[0].user;
-            password = res[0].password;
-            connection.release();
-            if (error) throw error;
-        })
-    })
-}
 
 module.exports = router;
