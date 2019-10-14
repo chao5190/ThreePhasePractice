fb00051eb791ab8624ce6c7330f32e4b788963dd
lijichao <1159165064@qq.com>
2019-10-14 17:27:12 +0800

    webpack+loader

---
+++ vue/day1/tmp/foo/app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+//读取相关路由模块
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login')
+var registerRouter = require('./routes/register')
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json()); //把数据转化为json数据
+app.use(express.urlencoded({
+  extended: false
+})); //避免出现中文乱码或其他编码乱码
+app.use(cookieParser());
+//读取相关静态资源
+app.use((req, res, next) => {
+  // 允许所有请求跨域，所有请求的响应头部分都带上cors
+  res.append('Access-Control-Allow-Origin', '*')
+  next()
+})
+app.use(express.static(path.join(__dirname, 'public')));
+// 根据相关路由读取相关路由模块
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/login", loginRouter);
+app.use("/register", registerRouter);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ vue/day1/webpack/src/index.js
@@ -0,0 +1,6 @@
+require("./sign.css");
+require("./style.css")
+require("./bootstrap.css")
+import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
