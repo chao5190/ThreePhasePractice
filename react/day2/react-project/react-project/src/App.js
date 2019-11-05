63e5e22712b9e7c2cfd104d1c39d5afea602ef42
lijichao <1159165064@qq.com>
2019-11-05 21:02:06 +0800

    react

---
+++ react/day2/react-project/react-project/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+import MyLayout from './pages/MyLayout/MyLayout'
+
+function App() {
+  return ( <div className = "App">
+    <MyLayout/>
+    </div>
+  );
+}
+
+export default App;
